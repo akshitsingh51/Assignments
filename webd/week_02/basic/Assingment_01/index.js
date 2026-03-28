fdbd3ee9d57a44024f90d351026ef4f26a356ad1
shivvay5 <220584278+shivvay5@users.noreply.github.com>
2026-03-28 11:11:03 +0530

    Completed the HTML CSS Assignment and Javascript Assighment

--- webd/week_02/basic/Assingment_01/index.js
+++
@@ -1,6 +0,0 @@
-// Write a function sum that finds the sum of two numbers.
-function Sum(a, b) {
-    console.log(a + b);
-}
-
-Sum(10, 20);
--- webd/week_02/basic/Assingment_02/index.js
+++
@@ -1,9 +0,0 @@
-// Write a function called canVote that returns true or false if the age of a user is > 18
-
-function canVote(age) {
-    if(age >= 18) return true;
-    else return false;
-}
-
-a = canVote(19);
-console.log(a);
